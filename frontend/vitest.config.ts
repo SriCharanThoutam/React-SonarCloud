f78c3604ad67859a9816bceb3033d51e4fb30f73
SriCharanThoutam <sricharan.thoutam@zemosolabs.com>
2025-08-29 02:35:34 +0530

    fix: vitest.config.ts and sonar-project.properties files

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-        globals: true
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage'
     }
+  }
 });
